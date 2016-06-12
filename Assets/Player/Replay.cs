925e70565d0c8f5a09b86c38d94bb459c95cf9fc
Stefan Worzalla <stewo1337@gmail.com>
2016-06-13 02:02:20 +0700

    Added Replay.cs and added Constructor to create keyframes for the replay function

---
+++ Assets/Player/Replay.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Replay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public struct MyKeyFrame {
+
+	public float frameTime;
+	public Vector3 position;
+	public Quaternion rotation;
+
+	public MyKeyFrame(float time, Vector3 pos, Quaternion rot) {
+		frameTime = time;
+		position = pos;
+		rotation = rot;
+	}
+}
